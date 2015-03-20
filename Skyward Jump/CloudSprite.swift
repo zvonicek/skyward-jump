8e97b7752c6da2ddae3c370ff797ba9e56a63105
Kieu Thi Nguyen <kieung@hotmail.com>
2015-03-20 20:21:55 +0100

    added functions for platform physics and creating platforms

--- Skyward Jump/CloudSprite.swift
+++ Skyward Jump/CloudSprite.swift
@@ -8,13 +8,38 @@
 
 import SpriteKit
 
-class CloudSprite: SKSpriteNode, NSCoding {
-    required convenience init(coder aDecoder: NSCoder) {
-        self.init()
-        //TODO
+class CloudSprite: SKNode {
+    
+    let platformSprite = SKSpriteNode(imageNamed: "platform.png")
+    
+    
+    //Adds physics to platforms
+    func platformPhysics() {
+        
+        
+        platformSprite.physicsBody = SKPhysicsBody(rectangleOfSize: platformSprite.size)
+        platformSprite.physicsBody?.dynamic = false
+        
+        platformSprite.physicsBody?.categoryBitMask = Categories.PlatformCategory
+        platformSprite.physicsBody?.collisionBitMask = Categories.PlatformCategory
+        
     }
     
-    override func encodeWithCoder(aCoder: NSCoder) {
-        //TODO
+   
+    //Function only create 1 platform at a fixed posistion
+    func createPlatforms() -> SKNode {
+        
+        platformSprite.position = CGPointMake(200, 100);
+        platformSprite.xScale = 0.5
+        platformSprite.yScale = 0.5
+        platformPhysics()
+        
+        self.addChild(platformSprite)
+        
+        return platformSprite
+        
     }
+    
+
+   
 }
