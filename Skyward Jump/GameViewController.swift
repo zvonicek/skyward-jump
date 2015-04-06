894c1986800ffce3577f28c8938fe952336c1338
Petr Zvoníček <zvonicek@gmail.com>
2015-04-06 22:50:32 +0200

    Set equal scene width on all devices

--- Skyward Jump/GameViewController.swift
+++ Skyward Jump/GameViewController.swift
@@ -19,10 +19,12 @@ class GameViewController: UIViewController, GameViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = multiplayerMode ? MultiplayerGameScene(size: self.view.bounds.size) : GameScene(size: self.view.bounds.size)
+        let height = UIScreen.mainScreen().bounds.height == 480 ? 480 : 568
+        let size = CGSizeMake(320, CGFloat(height))
+        let scene = multiplayerMode ? MultiplayerGameScene(size:size) : GameScene(size: size)
         scene.controllerDelegate = self
         let skView = view as SKView
-        scene.scaleMode = .ResizeFill
+        scene.scaleMode = .AspectFill
         skView.presentScene(scene)
     }
     
