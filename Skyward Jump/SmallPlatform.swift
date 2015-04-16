238707715ebaf0440f9037f2997a9f85c35ed29a
Petr Zvoníček <zvonicek@gmail.com>
2015-04-16 13:24:42 +0200

    Added SmallPlatform

---
+++ Skyward Jump/SmallPlatform.swift
@@ -0,0 +1,17 @@
+//
+//  SmallPlatform.swift
+//  Skyward Jump
+//
+//  Created by Petr Zvoníček on 16.04.15.
+//  Copyright (c) 2015 NTNU. All rights reserved.
+//
+
+class SmallPlatform: Platform {
+    override init(position: CGPoint) {
+        super.init(position: position, length: 100)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
