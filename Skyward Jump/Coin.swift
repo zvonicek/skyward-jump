4648055df993e0aef4f81ef570f37ef67b5b016f
Petr Zvoníček <zvonicek@gmail.com>
2015-04-19 15:52:24 +0200

    Added Coin model

---
+++ Skyward Jump/Coin.swift
@@ -0,0 +1,26 @@
+//
+//  Coin.swift
+//  Skyward Jump
+//
+//  Created by Petr Zvoníček on 19.04.15.
+//  Copyright (c) 2015 NTNU. All rights reserved.
+//
+
+import Foundation
+
+class Coin: NSObject, NSCoding {
+    
+    var position: CGPoint
+    
+    init(position: CGPoint) {
+        self.position = position
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        self.position = aDecoder.decodeCGPointForKey("pos")
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeCGPoint(self.position, forKey: "pos")
+    }
+}
