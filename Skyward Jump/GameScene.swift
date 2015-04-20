59022d87985b20bf05bb11e95f32312673f05b74
Petr Zvoníček <zvonicek@gmail.com>
2015-04-20 13:14:40 +0200

    GameScene: fixed HUD positions

--- Skyward Jump/GameScene.swift
+++ Skyward Jump/GameScene.swift
@@ -106,7 +106,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.createPlayer(playerStartHeight)
         
         //Configure pause-button
-        pauseButton.frame = CGRectMake(self.size.width * 0.9, 10, 30, 30)
+        pauseButton.frame = CGRectMake(UIScreen.mainScreen().bounds.size.width - 40, UIScreen.mainScreen().bounds.size.width == 320 ? 8 : 14, 30, 30)
         pauseButton.setImage(pauseIcon, forState: .Normal)
         pauseButton.addTarget(self, action: "pauseGame", forControlEvents: UIControlEvents.TouchUpInside)
         
@@ -120,7 +120,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 
         //Configure score-label
         scoreLabel.fontSize = 14
-        scoreLabel.position = CGPoint(x: self.size.width * 0.8, y: self.size.height - 30)
+        scoreLabel.position = CGPoint(x: self.size.width - 80, y: self.size.height - 30)
         scoreLabel.fontColor = SKColor.blackColor()
         scoreLabel.text = "Score: 0"
         
